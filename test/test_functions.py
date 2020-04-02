257ce89ea0d2b6f3a641be225eb09fd33183aaed
dergeh <richardgirke@aol.com>
2020-04-02 14:51:48 +0200

    fixed packaging

--- test/test_functions.py
+++ test/test_functions.py
@@ -1,3 +1,11 @@
+import sys
+import os
+
+module_path = os.path.abspath("..") 
+if module_path not in sys.path: 
+    sys.path.append(module_path)
+
+
 from nfp.scan import scan_parallel
 from nfp.reduce import reduce
 from nfp.common import read_groups
@@ -6,14 +14,13 @@ import pandas
 import q6_fn as q6
 
 
-
-scan_parallel("./resources/groups.parquet","./test/scanned",q6.filter,[[0,1]],2)
+scan_parallel("./resources/groups.parquet","./resources/scanned",q6.filter,[[0,1]],2)
 #print(read_groups("./test/test_scan.parquet",[0,1]).shape)
 
 
-map_parallel("./test/scanned", "./test/mapped",q6.mapper,[0,1],2 )
+map_parallel("./resources/scanned", "./resources/mapped",q6.mapper,[0,1],2 )
 #print(read_groups("./test/test_map.parquet",[0,1]).shape)
 
 
-reduce("./test/mapped","./test/test_reduce.parquet",q6.reducer,[0,1],1)
+reduce("./resources/mapped","./resources/test_reduce.parquet",q6.reducer,[0,1],1)
 #print(read_groups("./test/test_reduce.parquet",[0]))
